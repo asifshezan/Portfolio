0074f9ee957cf558729f4a72d29833a9220cdc90
Asif Ahamed <asifshezan7@gmail.com>
2022-08-19 12:37:41 +0600

    contact model and controller done

---
+++ database/migrations/2022_08_19_061849_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id('cont_id');
+            $table->string('cont_title')->nullable();
+            $table->string('cont_subtitle')->nullable();
+            $table->string('cont_details')->nullable();
+            $table->string('cont_button')->nullable();
+            $table->integer('cont_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
