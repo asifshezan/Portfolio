484a6e1bedbcf566d7b72e089316142406a34ea7
Asif Ahamed <asifshezan7@gmail.com>
2022-08-24 22:09:08 +0600

    education controller and model create

---
+++ database/migrations/2022_08_24_155508_create_education_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEducationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id('edu_id');
+            $table->string('edu_page_title')->nullable();
+            $table->string('edu_page_subtitle')->nullable();
+            $table->integer('edu_year')->nullable();
+            $table->string('edu_image',50)->nullable();
+            $table->string('edu_title')->nullable();
+            $table->string('edu_subtitle')->nullable();
+            $table->string('edu_text')->nullable();
+            $table->string('edu_slug',50)->nullable();
+            $table->integer('edu_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('education');
+    }
+}
