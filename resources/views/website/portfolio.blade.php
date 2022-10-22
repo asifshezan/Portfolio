f4123c688c7c62118aa1517d007080a8e873f607
Asif Ahamed <asifshezan7@gmail.com>
2022-10-22 09:18:44 +0600

    Portfolio page ready some bugs

--- resources/views/website/portfolio.blade.php
+++ resources/views/website/portfolio.blade.php
@@ -10,73 +10,44 @@
 				</div>
 
 				<!-- Sorting -->
+                @php
+                $alldata = App\Models\Portfolio_Category::where('port_cate_status',1)->get();
+                @endphp
 				<div class="portfolio_categories button-group filters-button-group">
 					<ul>
 						<li class="portfolio_category active is-checked" data-filter="*">All</li>
-						<li class="portfolio_category" data-filter=".p_design">Design</li>
-						<li class="portfolio_category" data-filter=".p_photography">Photography</li>
-						<li class="portfolio_category" data-filter=".p_brand">Brand Identity</li>
+                        @foreach ($alldata as $data )
+                        <li class="portfolio_category" data-filter=".w_design">{{ $data->port_cate_title }}</li>
+                        @endforeach
 					</ul>
 				</div>
 
 				<div class="main_content_scroll mCustomScrollbar" data-mcs-theme="minimal-dark">
 					<div class="portfolio_grid grid clearfix">
 
-						<!-- Portfolio Item -->
-						<div class="grid-item portfolio_item p_design">
-							<img src="{{asset('contents/website')}}/images/portfolio_1.jpg" alt="">
+                        @foreach ($alldata as $data)
+                            <!-- Portfolio Item -->
+						<div class="grid-item portfolio_item w_design">
+							<img src="{{ asset('uploads/portfolio_category/'.$data->port_cate_image)}}" alt="">
 							<div class="portfolio_item_content d-flex flex-column align-items-center justify-content-center">
 								<div class="portfolio_item_title">Design</div>
 								<div class="portfolio_item_link"><a href="#">See More</a></div>
 							</div>
 						</div>
-
-						<!-- Portfolio Item -->
-						<div class="grid-item portfolio_item p_photography">
-							<img src="{{asset('contents/website')}}/images/portfolio_2.jpg" alt="">
-							<div class="portfolio_item_content d-flex flex-column align-items-center justify-content-center">
-								<div class="portfolio_item_title">Photography</div>
-								<div class="portfolio_item_link"><a href="#">See More</a></div>
-							</div>
-						</div>
-
-						<!-- Portfolio Item -->
-						<div class="grid-item portfolio_item p_brand">
-							<img src="{{asset('contents/website')}}/images/portfolio_3.jpg" alt="">
-							<div class="portfolio_item_content d-flex flex-column align-items-center justify-content-center">
-								<div class="portfolio_item_title">Brand Identity</div>
-								<div class="portfolio_item_link"><a href="#">See More</a></div>
-							</div>
-						</div>
-
-						<!-- Portfolio Item -->
-						<div class="grid-item portfolio_item p_design">
-							<img src="{{asset('contents/website')}}/images/portfolio_4.jpg" alt="">
-							<div class="portfolio_item_content d-flex flex-column align-items-center justify-content-center">
-								<div class="portfolio_item_title">Design</div>
-								<div class="portfolio_item_link"><a href="#">See More</a></div>
-							</div>
-						</div>
-
-						<!-- Portfolio Item -->
-						<div class="grid-item portfolio_item p_photography">
-							<img src="{{asset('contents/website')}}/images/portfolio_5.jpg" alt="">
-							<div class="portfolio_item_content d-flex flex-column align-items-center justify-content-center">
-								<div class="portfolio_item_title">Photography</div>
-								<div class="portfolio_item_link"><a href="#">See More</a></div>
-							</div>
-						</div>
-
-						<!-- Portfolio Item -->
-						<div class="grid-item portfolio_item p_brand">
-							<img src="{{asset('contents/website')}}/images/portfolio_6.jpg" alt="">
-							<div class="portfolio_item_content d-flex flex-column align-items-center justify-content-center">
-								<div class="portfolio_item_title">Brand Identity</div>
-								<div class="portfolio_item_link"><a href="#">See More</a></div>
-							</div>
-						</div>
+                        @endforeach
 					</div>
-
 				</div>
 			</div>
             @endsection
+
+
+
+
+            {{-- <!-- Portfolio Item -->
+						<div class="grid-item portfolio_item w_dev">
+							<img src="{{ asset('uploads/portfolio_category/'.$data->port_cate_image)}}" alt="">
+							<div class="portfolio_item_content d-flex flex-column align-items-center justify-content-center">
+								<div class="portfolio_item_title">Photography</div>
+								<div class="portfolio_item_link"><a href="#">See More</a></div>
+							</div>
+						</div> --}}
