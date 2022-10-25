917ffce4c45fb651a6969e5f9e901f42eec9b179
Asif Ahamed <asifshezan7@gmail.com>
2022-10-25 20:45:02 +0600

    Service Page create

---
+++ resources/views/admin/services/create.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.admin')
+@section('content')
+<!-- start page title -->
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+            <h4 class="mb-sm-0 font-size-18">Services</h4>
+
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Services</a></li>
+                    <li class="breadcrumb-item active">Create Services</li>
+                </ol>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card border border-primary">
+            <div class="card-header bg-transparent border-primary d-flex justify-content-between">
+                <h5 class="my-0 text-primary align-middle"><i class="mdi mdi-bullseye-arrow me-3"></i>Create Services </h5>
+                <a href="{{ route('service.index') }}" class="btn btn-sm btn-primary waves-effect waves-light">
+                    <i class="bx bx-list-plus font-size-20 align-middle me-2"></i> All Services</a>
+            </div>
+            <div class="card-body">
+                <div class="card-body">
+                    <form action="{{ route('service.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row form-group">
+                            <div class="col-md-6 my-2">
+                                <label for="ser_page_title">Service Page Title</label>
+                                <input class="form-control" type="text" name="ser_page_title" value="{{ old('ser_page_title') }}">
+                                @error('ser_page_title')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 my-2">
+                                <label for="ser_page_subtitle">Service Page Sub-Title</label>
+                                <input class="form-control" type="text" name="ser_page_subtitle" value="{{ old('ser_page_subtitle') }}">
+                                @error('ser_page_subtitle')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 my-2">
+                                <label for="ser_icon">Service Icon</label>
+                                <input class="form-control" type="text" name="ser_icon" value="{{ old('ser_icon') }}">
+                                @error('ser_icon')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 my-2">
+                                <label for="ser_title">Service Title</label>
+                                <input class="form-control" type="text" name="ser_title" value="{{ old('ser_title') }}">
+                                @error('ser_title')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-md-7 my-2">
+                                <label for="ser_text">Service Text</label>
+                                {{-- <textarea cols="135" rows="4" name="ab_details" value="{{ old('ab_details') }}"></textarea> --}}
+                                <input class="form-control" type="text" name="ser_text" value="{{ old('ser_text') }}">
+                                @error('ser_text')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                        <div class="col-md-6 mt-4">
+                            <button type="submit" class="btn btn-primary waves-effect waves-light">
+                                <i class="bx bxs-save font-size-16 align-middle me-2"></i> Services Save
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/services/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.admin')
+@section('content')
+<!-- start page title -->
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+            <h4 class="mb-sm-0 font-size-18">Services</h4>
+
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Services</a></li>
+                    <li class="breadcrumb-item active">Edit Services</li>
+                </ol>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card border border-primary">
+            <div class="card-header bg-transparent border-primary d-flex justify-content-between">
+                <h5 class="my-0 text-primary align-middle"><i class="mdi mdi-bullseye-arrow me-3"></i>Edit Services </h5>
+                <a href="{{ route('service.index') }}" class="btn btn-sm btn-primary waves-effect waves-light">
+                    <i class="bx bx-list-plus font-size-20 align-middle me-2"></i> All Services
+                </a>
+            </div>
+            <div class="card-body">
+                <div class="card-body">
+                    <form action="{{ route('service.update',$data->ser_slug) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="row form-group">
+                        <div class="col-md-6 my-2">
+                            <label for="ser_page_title">Service Page Title</label>
+                            <input type="hidden" name="ser_id" value="{{ $data->ser_id }}">
+                            <input class="form-control" type="text" name="ser_page_title" value="{{ $data->ser_page_title }}">
+                            @error('ser_page_title')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-md-6 my-2">
+                            <label for="ser_page_subtitle">Service Page Sub-Title</label>
+                            <input class="form-control" type="text" name="ser_page_subtitle" value="{{ $data->ser_page_subtitle }}">
+                            @error('ser_page_subtitle')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-md-6 my-2">
+                            <label for="ser_icon">Service Icon</label>
+                            <input class="form-control" type="text" name="ser_icon" value="{{ $data->ser_icon }}">
+                            @error('ser_icon')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-md-6 my-2">
+                            <label for="ser_title">Service Title</label>
+                            <input class="form-control" type="text" name="ser_title" value="{{ $data->ser_title }}">
+                            @error('ser_title')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-md-6 my-2">
+                            <label for="ser_text">Service Text</label>
+                            <input class="form-control" type="text" name="ser_text" value="{{ $data->ser_text }}">
+                            @error('ser_text')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-md-7 mt-4">
+                            <button type="submit" class="btn btn-primary waves-effect waves-light">
+                                <i class="bx bxs-save font-size-16 align-middle me-2"></i> Updata Services
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
