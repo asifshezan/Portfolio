a5ded90f3d4074fea11109ec62a1775784862b74
Asif Ahamed <asifshezan7@gmail.com>
2022-10-28 21:19:21 +0600

    Basic_Info update done

--- app/Http/Controllers/Admin/Basic_InfoController.php
+++ app/Http/Controllers/Admin/Basic_InfoController.php
@@ -21,4 +21,39 @@ class Basic_InfoController extends Controller
         $basic_all = Basic_Info::where('info_status',1)->orderBy('info_id','DESC')->firstOrFail();
         return view('admin.Basic_Info.basic_info', compact('basic_all'));
     }
+
+    public function update(Request $request){
+        $update = Basic_Info::where('info_status',1)->where('info_id',1)->update([
+            'info_name' => $request['info_name'],
+            'info_location' => $request['info_location'],
+            'info_birth' => $request['info_birth'],
+            'info_email' => $request['info_email'],
+            'info_phone' => $request['info_phone'],
+            'info_website' => $request['info_website'],
+            'info_facebook' => $request['info_facebook'],
+            'info_instagram' => $request['info_instagram'],
+            'info_linkedin' => $request['info_linkedin'],
+            'info_google' => $request['info_google'],
+            'info_status' => 1,
+            'created_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        if($request->hasFile('info_image')){
+            $image = $request->file('info_image');
+            $imageName = time() . '-' . $image->getClientOriginalExtension();
+            Image::make($image)->save('uploads/basics/' . $imageName);
+
+            Basic_Info::where('info_id',1)->update([
+                'info_image' => $imageName
+            ]);
+        }
+
+        if($update){
+            Session::flash('success', 'Successfully updated');
+            return redirect()->back();
+        }else{
+            Session::flash('error', 'Failed to updated');
+            return redirect()->back();
+        }
+    }
 }
