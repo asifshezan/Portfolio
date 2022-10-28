51e422687d155a9f7b1c06d0a3ba8c25005b45f2
Asif Ahamed <asifshezan7@gmail.com>
2022-10-28 21:26:08 +0600

    Dashboard edit

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,24 +1,21 @@
 @extends('layouts.admin')
 @section('content')
 
-                     <!-- start page title -->
-                     <div class="row">
+                    <!-- start page title -->
+                    <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                                 <h4 class="mb-sm-0 font-size-18">Dashboard</h4>
-
                                 <div class="page-title-right">
                                     <ol class="breadcrumb m-0">
                                         <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
                                         <li class="breadcrumb-item active">Dashboard</li>
                                     </ol>
                                 </div>
-
                             </div>
                         </div>
                     </div>
                     <!-- end page title -->
-
                     <div class="row">
                         <div class="col-xl-3 col-md-6">
                             <!-- card -->
@@ -32,19 +29,10 @@
                                                 $<span class="counter-value" data-target="865.2">0</span>k
                                             </h4>
                                         </div>
-
-                                        <div class="col-6">
-                                            <div id="mini-chart1" data-colors='["#5156be"]' class="apex-charts mb-2"></div>
-                                        </div>
-                                    </div>
-                                    <div class="text-nowrap">
-                                        <span class="badge bg-soft-success text-success">+$20.9k</span>
-                                        <span class="ms-1 text-muted font-size-13">Since last week</span>
                                     </div>
                                 </div><!-- end card body -->
                             </div><!-- end card -->
                         </div><!-- end col -->
-
                         <div class="col-xl-3 col-md-6">
                             <!-- card -->
                             <div class="card card-h-100">
@@ -57,18 +45,10 @@
                                                 <span class="counter-value" data-target="6258">0</span>
                                             </h4>
                                         </div>
-                                        <div class="col-6">
-                                            <div id="mini-chart2" data-colors='["#5156be"]' class="apex-charts mb-2"></div>
-                                        </div>
-                                    </div>
-                                    <div class="text-nowrap">
-                                        <span class="badge bg-soft-danger text-danger">-29 Trades</span>
-                                        <span class="ms-1 text-muted font-size-13">Since last week</span>
                                     </div>
                                 </div><!-- end card body -->
                             </div><!-- end card -->
                         </div><!-- end col-->
-
                         <div class="col-xl-3 col-md-6">
                             <!-- card -->
                             <div class="card card-h-100">
@@ -81,18 +61,10 @@
                                                 $<span class="counter-value" data-target="4.32">0</span>M
                                             </h4>
                                         </div>
-                                        <div class="col-6">
-                                            <div id="mini-chart3" data-colors='["#5156be"]' class="apex-charts mb-2"></div>
-                                        </div>
-                                    </div>
-                                    <div class="text-nowrap">
-                                        <span class="badge bg-soft-success text-success">+ $2.8k</span>
-                                        <span class="ms-1 text-muted font-size-13">Since last week</span>
                                     </div>
                                 </div><!-- end card body -->
                             </div><!-- end card -->
                         </div><!-- end col -->
-
                         <div class="col-xl-3 col-md-6">
                             <!-- card -->
                             <div class="card card-h-100">
@@ -105,20 +77,9 @@
                                                 <span class="counter-value" data-target="12.57">0</span>%
                                             </h4>
                                         </div>
-                                        <div class="col-6">
-                                            <div id="mini-chart4" data-colors='["#5156be"]' class="apex-charts mb-2"></div>
-                                        </div>
-                                    </div>
-                                    <div class="text-nowrap">
-                                        <span class="badge bg-soft-success text-success">+2.95%</span>
-                                        <span class="ms-1 text-muted font-size-13">Since last week</span>
                                     </div>
                                 </div><!-- end card body -->
                             </div><!-- end card -->
                         </div><!-- end col -->
                     </div><!-- end row-->
-
-
-
-
 @endsection
